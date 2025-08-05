8b4d5d843fe8782cd6d9b55af7479da10f2d5537
Mauricio Corrêa da Silva <mauricio_correa_silva@hotmail.com>
2025-08-05 17:16:43 -0400

    Melhorias 2.0

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,5 +1,6 @@
 
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Versão 05 de agosto - 2.1 17:00 hs 
 
 let vetorAmigos = [];   //variavel array para a lista de amigos
 let numeroLimite = 4;   //numero maximo de amigos 
@@ -18,18 +19,24 @@ function adicionarAmigo()
       vetorAmigos.push(nome);                     //adiciona o nome no array
       document.querySelector('input').value = ''  //apenas limpa o campo
       atualizarLista()                            //imprime nomes na lista
- 
     };   
 }
   
 function sortearAmigo()
  {
+    if (vetorAmigos.length === 0) 
+    {
+       alert ('A lista de amigos está vazia, adicione amigos antes de sortear.')
+    }
+    else 
+    {    
     let quantidadeDeAmigosNaLista = vetorAmigos.length
     let indiceEscolhido = parseInt(Math.random() * numeroLimite + 1);
-    let resultado = vetorAmigos[indiceEscolhido];
- 
+    let nomeSorteado = vetorAmigos[indiceEscolhido];
+    
     //Exibir a mensagem com o resultado do amigo sorteado
-    document.getElementById('resultado').innerHTML = `O Amigo sorteado foi:${resultado}`;
+    document.getElementById('resultado').innerHTML = `O amigo secreto sorteado é:${nomeSorteado}`;
+    }
  }
 
  //Lógica para realizar a exibição dos nomes que foram escolhidos
@@ -37,7 +44,9 @@ function sortearAmigo()
  {
     const lista = document.getElementById('listaAmigos')
     lista.innerHTML = ""
-    for (let contador = 0; contador < vetorAmigos.length; contador++) {
+
+    for (let contador = 0; contador < vetorAmigos.length; contador++) 
+    {
         const item = document.createElement('li'); 
         item.textContent = vetorAmigos[contador];
         lista.appendChild(item);
