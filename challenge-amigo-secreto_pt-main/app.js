5a4ebd5990c5cfb82c9d5f24d5e3d812c866da6a
Mauricio Corrêa da Silva <mauricio_correa_silva@hotmail.com>
2025-08-05 16:31:44 -0400

    Correções 05 de Agosto

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,23 +1,45 @@
+
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let listaAmigos = [];
-let numeroLimite = 4;
+let vetorAmigos = [];   //variavel array para a lista de amigos
+let numeroLimite = 4;   //numero maximo de amigos 
 
 //Função acionada quando usuário clica adicionar amigo 
-function adicionarAmigo() {
+function adicionarAmigo() 
+{
     let nome = document.querySelector('input').value;
-    listaAmigos.push(nome);
-    exibirTextoNaTela ('ul', listaAmigos);
-}
 
-function sortearAmigo() {
-    let quantidadeDeAmigosNaLista = listaAmigos.length
+    if (nome == '')
+    {
+      alert('Por favor, insira um nome válido !');
+    }
+    else 
+    {
+      vetorAmigos.push(nome);                     //adiciona o nome no array
+      document.querySelector('input').value = ''  //apenas limpa o campo
+      atualizarLista()                            //imprime nomes na lista
+ 
+    };   
+}
+  
+function sortearAmigo()
+ {
+    let quantidadeDeAmigosNaLista = vetorAmigos.length
     let indiceEscolhido = parseInt(Math.random() * numeroLimite + 1);
-    let resultado = listaAmigos[indiceEscolhido];
-    exibirTextoNaTela ('ul', resultado );
-}
+    let resultado = vetorAmigos[indiceEscolhido];
+ 
+    //Exibir a mensagem com o resultado do amigo sorteado
+    document.getElementById('resultado').innerHTML = `O Amigo sorteado foi:${resultado}`;
+ }
 
-function exibirTextoNaTela(tag, texto) {
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto ;
-}
+ //Lógica para realizar a exibição dos nomes que foram escolhidos
+ function atualizarLista() 
+ {
+    const lista = document.getElementById('listaAmigos')
+    lista.innerHTML = ""
+    for (let contador = 0; contador < vetorAmigos.length; contador++) {
+        const item = document.createElement('li'); 
+        item.textContent = vetorAmigos[contador];
+        lista.appendChild(item);
+     }
+ }
